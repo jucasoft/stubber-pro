70ab22cc817c77dec14449a252ca63c8607e6589
jucasoft <lluuccaa1@gmail.com>
2023-10-27 17:19:23 +0200

    Update app.component.ts

--- angular-app-to-test-lib/src/app/app.component.ts
+++ angular-app-to-test-lib/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'stubber-pro-nx-root',
+  selector: 'stubber-pro-mr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-app-to-test-lib';
+export class AppComponent implements OnInit{
+  title = 'angular-application-to-test-library';
+
+  users:any[] = []
+  profiles:any[] = []
+  products:any[] = []
+
+  constructor(private http:HttpClient) {
+
+  }
+
+  call(path:string): void {
+    this.http.get(path).subscribe(
+      (values:any) => {
+        (values as any[]).slice(0, 2).forEach((value:any, i:number) => {
+          this.http.post(path, {...value, id: new Date().getTime()+i}).subscribe()
+        })
+      }
+    )
+  }
+
+  ngOnInit(): void {
+    ["/api/v1/users","/api/v1/profiles","/api/v1/products"].forEach(path => this.call(path))
+  }
+
+
 }
