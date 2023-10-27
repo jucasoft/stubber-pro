92e43025f55debc75ff0bcecfad8df183499e752
jucasoft <lluuccaa1@gmail.com>
2023-10-27 16:37:33 +0200

    proxyConfig

--- stubber-pro-app/src/app/app.module.ts
+++ stubber-pro-app/src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { TableModule } from 'primeng/table';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    DropdownModule,
+    TableModule,
+    ToggleButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
