75d1b17b26fdb7461b1f99ee0fa94a644cdf0516
jucasoft <lluuccaa1@gmail.com>
2023-10-28 10:53:45 +0200

    Update stubber-pro-options.ts

--- stubber-pro-lib/src/lib/models/stubber-pro-options.ts
+++ stubber-pro-lib/src/lib/models/stubber-pro-options.ts
@@ -1,15 +1,16 @@
 import {Filter} from "../path-filter";
 import {IncomingMessage} from "http";
 
-export interface StubberProRouteOptions { routePath: string
-    pathFilter?: Filter<IncomingMessage>;
+export interface StubberProRouteOptions {
+  routePath: string
+  pathFilter?: Filter<IncomingMessage>;
 }
 
-export interface StubberProoptions {
-    basePath: string
-    appUri: string
-    apiUri: string
-    activeDefaultValue: boolean
-    routes: StubberProRouteOptions[]
-    getKey: (opt: StubberProoptions, req: any)=>string
+export interface StubberProOptions {
+  basePath: string
+  appUri: string
+  apiUri: string
+  activeDefaultValue: boolean // default value of the activer field, if set to true, the captured values will be immediately used as mocks
+  routes: StubberProRouteOptions[]
+  getKey:(opt: StubberProOptions, req: any)=>string
 }
