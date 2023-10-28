cfa201972e9500c41cdf9e34de99f29ce4264ff4
jucasoft <lluuccaa1@gmail.com>
2023-10-28 10:57:17 +0200

    Update route-base.ts

--- stubber-pro-lib/src/lib/models/route-base.ts
+++ stubber-pro-lib/src/lib/models/route-base.ts
@@ -1,22 +1,21 @@
+import {BaseController} from "./base-controller";
 import * as express from "express";
-import {CrudControllerBase} from "./crud-controller-base";
 
-export class RouteBase<T> {
-
+export class BaseRoute<T>{
     router = express.Router()
-
-    constructor(controller: CrudControllerBase<T>) {
-        this.router.post('/search', controller.search)
-        this.router.post('/select', controller.select)
-        this.router.post('/create', controller.create)
-        this.router.post('/createMany', controller.createMany)
-        this.router.post('/update', controller.update)
-        this.router.post('/updateMany', controller.updateMany)
-        this.router.post('/delete', controller.delete)
-        this.router.post('/deleteMany', controller.deleteMany)
+    constructor(controller: BaseController<T>) {
+        this.router.post(`/search`,controller.search)
+        this.router.post(`/select`,controller.select)
+        this.router.post(`/create`,controller.create)
+        this.router.post(`/createMany`,controller.createMany)
+        this.router.post(`/update`,controller.update)
+        this.router.post(`/updateMany`,controller.updateMany)
+        this.router.post(`/delete`,controller.delete)
+        this.router.post(`/deleteMany`,controller.deleteMany)
     }
 
-    getRouter() {
+    getRouter(){
         return this.router
     }
+
 }
