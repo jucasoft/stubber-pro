41e14686371777ac1d7c4f7f66f2612d47bca93c
jucasoft <lluuccaa1@gmail.com>
2023-12-19 15:36:23 +0100

    Update dump-file.module.ts

--- stubber-pro-app/src/app/root-store/dump-file-store/dump-file.module.ts
+++ stubber-pro-app/src/app/root-store/dump-file-store/dump-file.module.ts
@@ -1,1 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { featureReducer } from './dump-file.reducer';
+import { DumpFileEffects } from './dump-file.effects';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature('dumpFile', featureReducer),
+    EffectsModule.forFeature([DumpFileEffects]),
+  ],
+})
+export class DumpFileModule {}
 
