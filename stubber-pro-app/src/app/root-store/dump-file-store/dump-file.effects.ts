bd9b3c03aaf49cf659184cb608e67d48f4af33e9
jucasoft <lluuccaa1@gmail.com>
2023-12-19 15:35:42 +0100

    Create dump-file.effects.ts

---
+++ stubber-pro-app/src/app/root-store/dump-file-store/dump-file.effects.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {Action, Store} from '@ngrx/store';
+import {Observable, tap} from 'rxjs';
+import {mutationCall, mutationCatchError, mutationResponse} from "@core/async-crud-operation/aco-effect";
+import {dump} from "@root-store/dump-file-store/dump-file.actions";
+import {DumpFileService} from "@services/dump-file.service";
+import {repeat} from "rxjs/operators";
+
+@Injectable()
+export class DumpFileEffects {
+  uploadRequestEffect$: Observable<Action> = createEffect(() =>
+    this.actions$.pipe(
+      ofType(dump.Request),
+      tap(value => console.log("value: ", value)),
+      mutationCall(this.dumpFileService.dump),
+      mutationResponse(dump),
+      mutationCatchError(dump),
+      repeat()
+    )
+  )
+
+  constructor(
+    private dumpFileService: DumpFileService,
+    private actions$: Actions,
+    private store$: Store
+  ) {
+    console.log("DumpFileEffects.constructor()");
+  }
+
+}
