60bef713fa050a93f40890dd051e6356c98f0495
jucasoft <lluuccaa1@gmail.com>
2023-12-19 17:00:55 +0100

    stubber-pro-app 0.0.8-beta

--- stubber-pro-app/src/app/core/async-crud-operation/aco.actions.ts
+++ stubber-pro-app/src/app/core/async-crud-operation/aco.actions.ts
@@ -1,24 +1,42 @@
-import {ActionEnum, CrudEnum, OptRequest, OptResponse} from "ngrx-entity-crud";
-import {createAction, props} from '@ngrx/store';
-import {AcoActions} from "@core/async-crud-operation/aco-model";
+import {
+  ActionEnum,
+  CrudEnum,
+  OptRequest,
+  OptResponse,
+} from 'ngrx-entity-crud';
+import { createAction, props } from '@ngrx/store';
+import { AcoActions } from '@core/async-crud-operation/aco.model';
 
-export function createAcoActions<T>(name: string, actionName:string, suffix: string): AcoActions<T, OptRequest<T>, OptResponse<T>> {
-
-  const _suffix = suffix ? suffix + ' ' : ''
-  const Response = createAction(`[${name}] ${suffix}${ActionEnum.RESPONSE}`, props<OptResponse<T>>());
-  const Request = createAction(`[${name}] ${suffix}${actionName} ${ActionEnum.REQUEST}`, props<OptRequest<T>>());
-  const Failure = createAction(`[${name}] ${suffix}${actionName} ${ActionEnum.FAILURE}`, props<{ error: string }>());
-  const Success = createAction(`[${name}] ${suffix}${actionName} ${ActionEnum.SUCCESS}`, props<{
-    data: T,
-    request: OptRequest<T>
-  }>());
+export function createAcoActions<T>(
+  name: string,
+  actionName: string,
+  suffix: string
+): AcoActions<T, OptRequest<T>, OptResponse<T>> {
+  const _suffix = suffix ? suffix + ' ' : '';
+  const Response = createAction(
+    `[${name}] ${suffix}${ActionEnum.RESPONSE}`,
+    props<OptResponse<T>>()
+  );
+  const Request = createAction(
+    `[${name}] ${suffix}${actionName} ${ActionEnum.REQUEST}`,
+    props<OptRequest<T>>()
+  );
+  const Failure = createAction(
+    `[${name}] ${suffix}${actionName} ${ActionEnum.FAILURE}`,
+    props<{ error: string }>()
+  );
+  const Success = createAction(
+    `[${name}] ${suffix}${actionName} ${ActionEnum.SUCCESS}`,
+    props<{
+      data: T;
+      request: OptRequest<T>;
+    }>()
+  );
 
   return {
     Response,
     Request,
     Failure,
-    Success
+    Success,
   };
-
 }
-
