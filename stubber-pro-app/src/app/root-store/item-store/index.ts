ffc83bed7dafb2ee4e3d79fe343e798718525691
jucasoft <lluuccaa1@gmail.com>
2023-12-19 15:40:14 +0100

    Update index.ts

---
+++ stubber-pro-app/src/app/root-store/item-store/index.ts
@@ -0,0 +1,14 @@
+import * as ItemStoreActions from './item.actions';
+import * as ItemStoreSelectors from './item.selectors';
+
+import * as ItemStoreState from './item.state';
+
+export {
+	ItemStoreModule
+} from './item-store.module';
+
+export {
+	ItemStoreActions,
+	ItemStoreSelectors,
+	ItemStoreState
+};
