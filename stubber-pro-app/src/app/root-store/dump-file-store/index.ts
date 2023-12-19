49e4c038c5bc8e60c9b256bf128488f6b5e7b17f
jucasoft <lluuccaa1@gmail.com>
2023-12-19 15:39:32 +0100

    Update index.ts

---
+++ stubber-pro-app/src/app/root-store/dump-file-store/index.ts
@@ -0,0 +1,14 @@
+import * as DumpFileActions from './dump-file.actions';
+import * as DumpFileSelectors from './dump-file.selectors';
+
+import * as DumpFileState from './dump-file.state';
+
+export {
+	DumpFileModule
+} from './dump-file.module';
+
+export {
+	DumpFileActions,
+	DumpFileSelectors,
+	DumpFileState
+};
