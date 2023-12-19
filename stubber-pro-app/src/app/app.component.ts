2a035ed8fb8ba7eb94f139cfc76845568daf9b4d
jucasoft <lluuccaa1@gmail.com>
2023-12-19 15:46:59 +0100

    Update app.component.ts

--- stubber-pro-app/src/app/app.component.ts
+++ stubber-pro-app/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Store} from "@ngrx/store";
+import {ItemStoreActions, ItemStoreSelectors} from "@root-store/item-store/index";
+import {Observable} from "rxjs";
+import {Item} from "@models/vo/item";
+import {dump} from "@root-store/dump-file-store/dump-file.actions";
+import {DumpFileActions} from "@root-store/dump-file-store";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private readonly store$:Store) {}
 
   versions = [
     { name: 'master', code: 'master' },
@@ -17,8 +24,7 @@ export class AppComponent implements OnInit {
     { name: '4.3.1', code: '4.3.1' },
   ];
 
-  title = 'stubber-pro-app';
-  items: any[] = [];
+  items$: Observable<any[]>;
   statesList = [
     { name: '200', code: '200' },
     { name: '3xx', code: '3xx' },
@@ -27,47 +33,60 @@ export class AppComponent implements OnInit {
   ];
 
   ngOnInit(): void {
+    this.items$ = this.store$.select(ItemStoreSelectors.selectAll)
     this.reset();
   }
 
   prev() {}
 
   reset() {
-    this.http
-      .post<any[]>('api/v1/item/search', { id: 0 })
-      .subscribe((items: any[]) => (this.items = items));
+    this.store$.dispatch(ItemStoreActions.SearchRequest({mode:"REFRESH", queryParams:{}}))
+    // this.http
+    //   .post<any[]>('api/v1/item/search', { id: 0 })
+    //   .subscribe((items: any[]) => (this.items = items));
   }
 
   delete(item: any) {
-    this.http
-      .post<any[]>('api/v1/item/delete', item)
-      .subscribe((items: any[]) => (this.items = items));
+    this.store$.dispatch(ItemStoreActions.DeleteRequest({mutationParams:item}))
+
+    // this.http
+    //   .post<any[]>('api/v1/item/delete', item)
+    //   .subscribe();
+
+    // this.http
+    //   .post<any[]>('api/v1/item/delete', item)
+    //   .subscribe((items: any[]) => (this.items = items));
   }
 
   dump() {
-    this.http
-      .post<any[]>('api/v1/utils/dump', {})
-      .subscribe((items: any[]) => (this.items = items));
+    this.store$.dispatch(DumpFileActions.dump.Request({mutationParams:{}}))
+
+    // this.http
+    //   .post<any[]>('api/v1/utils/dump', {})
+    //   .subscribe((items: any[]) => (console.log("items: ", items)));
   }
 
   restore() {
-    this.http
-      .post<any[]>('api/v1/utils/restore', {})
-      .subscribe((items: any[]) => {
-        (this.items = items)
-      });
+    // this.http
+    //   .post<any[]>('api/v1/utils/restore', {})
+    //   .subscribe((items: any[]) => {
+    //     (this.items = items)
+    //   });
   }
 
   add() {
-    this.http
-      .post<any[]>('api/v1/add', {})
-      .subscribe((items: any[]) => (this.items = items));
+    // this.http
+    //   .post<any[]>('api/v1/add', {})
+    //   .subscribe((items: any[]) => (this.items = items));
   }
 
-  onActiveChange(item: any) {
-    this.http
-      .post<any[]>('api/v1/item/update', { ...item, active: !item.active })
-      .subscribe((items: any[]) => (this.items = items));
+  onActiveChange(item: Item) {
+    const mutationParams = {...item, active:!item.active}
+    this.store$.dispatch(ItemStoreActions.EditRequest({mutationParams}))
+
+    // this.http
+    //   .post<any[]>('api/v1/item/update', { ...item, active: !item.active })
+    //   .subscribe((items: any[]) => (this.items = items));
   }
 
   deleteAll(items:any[]) {
